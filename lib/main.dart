ab37d14bbb8de83c9139b8b82fb1225d03acfda4
unixkiwi <unixkiwi77@outlook.com>
2025-06-13 17:59:42 +0200

    feat: reimplemented auth_check

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:school_app/data/repo/beste_schule_repo_impl.dart';
 import 'package:school_app/domain/repo/beste_schule_repo.dart';
-import 'package:school_app/presentation/pages/login_page/auth_checker.dart';
 import 'package:school_app/presentation/pages/grades_page/grades_page.dart';
+import 'package:school_app/presentation/pages/login_page/auth_checker.dart';
 import 'package:school_app/presentation/pages/timetable_page/lessons_week_page.dart';
 import 'package:school_app/presentation/viewmodels/grades_page_viewmodel.dart';
 import 'package:school_app/presentation/viewmodels/timetable_page_viewmodel.dart';
@@ -47,64 +47,10 @@ class SchoolApp extends StatelessWidget {
             ),
 
             // auth check page to check wether the token is in storage
-            home:  MainNavBar() //const AuthChecker(),
+            home:  const AuthChecker(),
           ),
         );
       },
-    );
-  }
-}
-
-// New main navigation widget
-class MainNavBar extends StatefulWidget {
-  @override
-  State<MainNavBar> createState() => _MainNavBarState();
-}
-
-class _MainNavBarState extends State<MainNavBar> {
-  int _selectedIndex = 0;
-
-  static final List<Widget> _pages = [
-    ChangeNotifierProvider(
-      create:
-          (context) => TimetablePageViewmodel(
-            repo: Provider.of<BesteSchuleRepo>(context, listen: false),
-          ),
-      child: TimetablePage(),
-    ),
-    ChangeNotifierProvider(
-      create:
-          (context) => GradesPageViewmodel(
-            repo: Provider.of<BesteSchuleRepo>(context, listen: false),
-          ),
-      child: GradesPage(),
-    ),
-    // Settings page is not implemented, show a placeholder
-    Center(child: Text('Settings page coming soon')),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onItemTapped,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.calendar_today),
-            label: 'Timetable',
-          ),
-          NavigationDestination(icon: Icon(Icons.grade), label: 'Grades'),
-          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
-        ],
-      ),
     );
   }
 }
--- lib/presentation/pages/login_page/auth_checker.dart
+++ lib/presentation/pages/login_page/auth_checker.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:school_app/data/models/beste_schule_oauth_client_impl.dart';
 import 'package:school_app/domain/repo/beste_schule_repo.dart';
 import 'package:school_app/presentation/pages/login_page/login_page.dart';
-import 'package:school_app/presentation/pages/overview_page/overview_page.dart';
+import 'package:school_app/presentation/pages/main_navbar/main_navbar.dart';
 import 'package:school_app/presentation/viewmodels/login_page_viewmodel.dart';
 import 'package:school_app/presentation/viewmodels/overview_page_viewmodel.dart';
 import 'package:school_app/utils/logger.dart';
@@ -28,7 +28,7 @@ class AuthChecker extends StatelessWidget {
                   (context) => OverviewPageViewmodel(
                     repo: context.read<BesteSchuleRepo>(),
                   ),
-              child: const OverviewPage(),
+              child: const MainNavBar(),
             );
           }
         }
