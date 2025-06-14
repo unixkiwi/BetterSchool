6ea88acc4b0dd32c05f2097357b21fcb97914abd
unixkiwi <unixkiwi77@outlook.com>
2025-06-14 20:14:55 +0200

    feat: update AuthChecker to use webview token retrieval and simplify token handling logic

--- lib/presentation/pages/login_page/auth_checker.dart
+++ lib/presentation/pages/login_page/auth_checker.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:school_app/data/models/oauth_repo_impl_oauth_client_pkg.dart';
+import 'package:school_app/data/models/oauth_repo_impl_webview.dart';
 import 'package:school_app/domain/repo/beste_schule_repo.dart';
 import 'package:school_app/presentation/pages/login_page/login_page.dart';
 import 'package:school_app/presentation/pages/main_navbar/main_navbar.dart';
 import 'package:school_app/presentation/viewmodels/login_page_viewmodel.dart';
-import 'package:school_app/presentation/viewmodels/overview_page_viewmodel.dart';
 import 'package:school_app/utils/logger.dart';
 
 class AuthChecker extends StatelessWidget {
@@ -14,23 +14,15 @@ class AuthChecker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: BesteSchuleOauthRepoImpl().helper.getTokenFromStorage(),
+      future: BesteSchuleOauthWebviewRepoImpl().getTokenFromStorage(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         }
 
         if (snapshot.hasData && snapshot.data != null) {
-          if (snapshot.data!.accessToken != null) {
-            logger.i("[AuthChecker] Found token in storage");
-            return ChangeNotifierProvider(
-              create:
-                  (context) => OverviewPageViewmodel(
-                    repo: context.read<BesteSchuleRepo>(),
-                  ),
-              child: const MainNavBar(),
-            );
-          }
+          logger.i("[AuthChecker] Found token in storage");
+          return const MainNavBar();
         }
 
         logger.i("[AuthChecker] No token found in storage");
