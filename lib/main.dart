7b9b1a47fa7adb43ef6c443aea8eebacc5a1cbeb
unixkiwi <unixkiwi77@outlook.com>
2025-06-23 17:23:24 +0200

    feat: replaced dynamic_color library with the fork dynamic_system_colors which fixed an issue with the elevation and background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dynamic_color/dynamic_color.dart';
+import 'package:dynamic_system_colors/dynamic_system_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:school_app/data/repo/beste_schule_repo_impl.dart';
