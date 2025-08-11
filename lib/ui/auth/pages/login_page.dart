ac50f4f01c901bc9093c3f1988c5c51c689ff878
unixkiwi <unixkiwi77@outlook.com>
2025-08-11 14:35:50 +0200

    feat: added login button with page + clean up
    UI only

---
+++ lib/ui/auth/pages/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:betterschool/ui/auth/widgets/login_btn.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: LoginButton());
+  }
+}
+
---
+++ lib/ui/auth/widgets/login_btn.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () {},
+      child: Text("Login with beste.schule"),
+    );
+  }
+}
