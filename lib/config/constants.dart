4b0a148efa1602429b1834db257d5c54df76b8f6
unixkiwi <unixkiwi77@outlook.com>
2025-08-12 18:53:27 +0200

    feat(constants): added constants

---
+++ lib/config/constants.dart
@@ -0,0 +1,10 @@
+final String tokenKey = "token";
+
+final String authorizeUrl = "https://beste.schule/oauth/authorize";
+final String tokenUrl = "https://beste.schule/oauth/token";
+
+final String customUrlScheme = "de.unixkiwi.betterschool";
+final String redirectUrl = "$customUrlScheme://oauth2redirect";
+
+final String clientId = "143";
+final String clientSecret = "N7isbO1oEezAkOCRKCHl8SZ5gti2T3IUkm5B1z7v";
