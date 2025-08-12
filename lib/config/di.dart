a2e62dd3a10ca160280a9fd1b8d3ca937618062c
unixkiwi <unixkiwi77@outlook.com>
2025-08-12 19:30:50 +0200

    feat(di): added new dependency injection

--- lib/config/di.dart
+++ lib/config/di.dart
@@ -1,8 +1,10 @@
 import 'package:betterschool/data/repositories/auth_repository.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt sl = GetIt.instance;
 
 void initDependencies() {
   sl.registerSingleton<AuthRepository>(AuthRepository());
+  sl.registerSingleton<FlutterSecureStorage>(FlutterSecureStorage());
 }
