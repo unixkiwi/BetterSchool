25d5c27ebc5bb4f375c1999001dbdc1db3a7f5ea
unixkiwi <unixkiwi77@outlook.com>
2025-08-13 15:26:27 +0200

    feat(pages): added grades, settings and timetable page
    They just got example text on them for the Home Page.

---
+++ lib/ui/grades/pages/grades_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GradesPage extends StatelessWidget {
+  const GradesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Grades Page"));
+  }
+}
---
+++ lib/ui/timetable/pages/timetable_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TimetablePage extends StatelessWidget {
+  const TimetablePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Timetable Page"));
+  }
+}
