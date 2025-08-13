e226e0bf3efad5e7a0d08b02ef389513a47b58c1
unixkiwi <unixkiwi77@outlook.com>
2025-08-13 15:33:57 +0200

    feat(routing): added home route

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,16 +1,19 @@
 import 'package:betterschool/ui/grades/pages/grades_page.dart';
+import 'package:betterschool/ui/home_navbar/pages/home_page.dart';
 import 'package:betterschool/ui/login/pages/login_page.dart';
 import 'package:betterschool/ui/settings/pages/settings_page.dart';
 import 'package:betterschool/ui/timetable/pages/timetable_page.dart';
 import 'package:flutter/widgets.dart';
 
 final String loginRoute = "/login";
+final String homeRoute = "/home";
 final String timetableRoute = "/timetable";
 final String gradesRoute = "/grades";
 final String settingsRoute = "/settings";
 
 final Map<String, WidgetBuilder> routes = {
   loginRoute: (BuildContext context) => const LoginPage(),
+  homeRoute: (BuildContext context) => const HomePageNavBar(),
   timetableRoute: (BuildContext context) => const TimetablePage(),
   gradesRoute: (BuildContext context) => const GradesPage(),
   settingsRoute: (BuildContext context) => const SettingsPage(),
