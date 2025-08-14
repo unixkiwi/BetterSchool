48cf4ce513a36dffe4a32274d2358ca620e80cbb
unixkiwi <unixkiwi77@outlook.com>
2025-08-14 15:31:29 +0200

    fix(android): wrong additional quote in build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "de.unixkiwi.betterschool"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973""
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
