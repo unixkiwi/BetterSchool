ca3d4388639288aa1d33d0cec4be087cb0a8ad16
unixkiwi <unixkiwi77@outlook.com>
2025-08-15 17:35:15 +0200

    feat: added new dependency injections

--- lib/config/di.dart
+++ lib/config/di.dart
@@ -1,6 +1,7 @@
 import 'package:betterschool/data/repositories/auth/auth_repository.dart';
 import 'package:betterschool/data/services/beste_schule_api/beste_schule_api_client_impl.dart';
 import 'package:betterschool/ui/timetable/pages/bloc/timetable_bloc.dart';
+import 'package:betterschool/utils/result.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
@@ -8,12 +9,32 @@ import 'package:get_it/get_it.dart';
 GetIt sl = GetIt.instance;
 
 void initDependencies() {
-  Dio dio = Dio();
-
-  sl.registerSingleton(dio);
-  sl.registerSingleton(BesteSchuleApiClientImpl(sl()));
   sl.registerSingleton<FlutterSecureStorage>(FlutterSecureStorage());
   sl.registerSingleton<AuthRepository>(AuthRepository(sl()));
+
+  Dio dio = Dio(
+    BaseOptions(
+      contentType: "application/json",
+      headers: {"Accept": "application/json"},
+    ),
+  );
+
+  sl.registerSingleton(dio);
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        Result<String> token = await sl<AuthRepository>().getToken();
+
+        if (token is Success<String>) {
+          options.headers['Authorization'] = 'Bearer ${token.value}';
+          return handler.next(options);
+        }
+      },
+    ),
+  );
+
+  sl.registerSingleton(BesteSchuleApiClientImpl(sl()));
 
   sl.registerFactory(() => TimetableBloc(sl()));
 }
