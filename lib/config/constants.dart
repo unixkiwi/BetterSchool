9f14f378d7ac3744e793a3c3d04de68c34e42d3a
unixkiwi <unixkiwi77@outlook.com>
2025-08-21 17:18:52 +0200

    feat(const): added constants for assets

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -14,3 +14,10 @@ const String besteSchuleApiBaseURL = "https://beste.schule/api";
 const int noInternetDioStatusCode = -6;
 const int unauthorizedStatusCode = 401;
 const int forbiddenStatusCode = 403;
+
+const String assetPrefix = "assets";
+const String noInternetSvg = "$assetPrefix/no_internet.svg";
+const String serverErrorSvg = "$assetPrefix/server_error.svg";
+const String noDataSvg = "$assetPrefix/no_data.svg";
+const String loginSvg = "$assetPrefix/login.svg";
+const String freetimeSvg = "$assetPrefix/freetime.svg";
