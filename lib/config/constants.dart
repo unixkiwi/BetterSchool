d86ec3c449ba8cbc5b8c939135d5879e864be541
unixkiwi <unixkiwi77@outlook.com>
2025-08-21 17:02:13 +0200

    feat(const): append to the last commit

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -12,3 +12,5 @@ const String clientSecret = "N7isbO1oEezAkOCRKCHl8SZ5gti2T3IUkm5B1z7v";
 const String besteSchuleApiBaseURL = "https://beste.schule/api";
 
 const int noInternetDioStatusCode = -6;
+const int unauthorizedStatusCode = 401;
+const int forbiddenStatusCode = 403;
