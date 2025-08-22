932606f431a5f684df953ab6d44663c2a799651a
unixkiwi <unixkiwi77@outlook.com>
2025-08-22 17:57:32 +0200

    feat(loading): implemented new cooler loading spinner

--- lib/ui/core/widgets/loading.dart
+++ lib/ui/core/widgets/loading.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingSpinner extends StatelessWidget {
   const LoadingSpinner({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
+    return Center(
+      child: LoadingAnimationWidget.staggeredDotsWave(
+        color: Theme.of(context).colorScheme.primary,
+        size: 100,
+      ),
+    );
   }
 }
