9486c1095db97581a747c57bfe54e64298bf35a5
unixkiwi <unixkiwi77@outlook.com>
2025-08-23 19:33:34 +0200

    feat(timetable,bloc): added week parameter to pageswitch event

--- lib/ui/timetable/bloc/timetable_event.dart
+++ lib/ui/timetable/bloc/timetable_event.dart
@@ -8,6 +8,7 @@ final class TimetablePageStartedEvent extends TimetableEvent {}
 final class TimetablePageSwitchEvent extends TimetableEvent {
   final int page;
   final bool isLastPage;
+  final WeekString weekString;
 
-  TimetablePageSwitchEvent({required this.page, required this.isLastPage});
+  TimetablePageSwitchEvent({required this.page, required this.isLastPage, required this.weekString});
 }
