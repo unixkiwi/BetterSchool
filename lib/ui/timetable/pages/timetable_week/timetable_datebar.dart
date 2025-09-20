066748bfd1d689fa7f4597f15d52f2ba9b4fe6df
unixkiwi <unixkiwi77@outlook.com>
2025-09-20 17:24:41 +0200

    style: reformat

--- lib/ui/timetable/pages/timetable_week/timetable_datebar.dart
+++ lib/ui/timetable/pages/timetable_week/timetable_datebar.dart
@@ -7,26 +7,26 @@ class TimetableDatebar extends StatelessWidget implements PreferredSizeWidget {
   const TimetableDatebar({super.key, required this.currentDay});
 
   @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
   Widget build(BuildContext context) {
     return AppBar(
       leading: IconButton(
         onPressed: () {},
         icon: Icon(Icons.keyboard_arrow_left_rounded),
       ),
+      title: TextButton(
+        onPressed: () {},
+        child: Text(currentDay?.date.toIso8601String() ?? "Loading..."),
+      ),
+      centerTitle: true,
       actions: [
         IconButton(
           onPressed: () {},
           icon: Icon(Icons.keyboard_arrow_right_rounded),
         ),
       ],
-      title: TextButton(
-        onPressed: () {},
-        child: Text(currentDay?.date.toIso8601String() ?? "Loading..."),
-      ),
-      centerTitle: true,
     );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
