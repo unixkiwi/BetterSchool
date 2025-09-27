2559f3658e2504c3474ec5e1333bd434cb50f05e
unixkiwi <unixkiwi77@outlook.com>
2025-09-27 20:02:29 +0200

    fix: fixed db creation error for caching

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
+import 'dart:io';
+
 import 'package:betterschool/config/di.dart';
 import 'package:betterschool/routing/routes.dart';
 import 'package:betterschool/ui/home_navbar/bloc/home_bloc.dart';
 import 'package:betterschool/ui/login/bloc/login_bloc.dart';
 import 'package:betterschool/ui/login/pages/login_page.dart';
+import 'package:betterschool/utils/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:remote_caching/remote_caching.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+Future<void> initializeRemoteCaching() async {
+  final directory = await getApplicationSupportDirectory();
+  final dbPath = join(directory.path, 'cache');
+
+  // Ensure directory exists
+  await Directory(directory.path).create(recursive: true);
+
+  await RemoteCaching.instance.init(databasePath: dbPath);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await RemoteCaching.instance.init(
-    defaultCacheDuration: Duration(days: 1),
-    verboseMode: true,
-  );
+  await initializeRemoteCaching();
+  // try {
+  //   await RemoteCaching.instance.init(
+  //     // defaultCacheDuration: Duration(days: 1),
+  //     verboseMode: true,
+  //     databasePath: join(await getDatabasesPath(), 'cache.db'),
+  //   );
+  // } catch (e) {
+  //   // imagine you got error handling
+  //   logger.e(e.toString());
+  // }
 
   initDependencies();
 
