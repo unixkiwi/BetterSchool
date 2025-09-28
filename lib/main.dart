2a980179c5ae62e67b6fa12f380dce3dfa6cae42
unixkiwi <unixkiwi77@outlook.com>
2025-09-28 18:19:39 +0200

    cleanup: imports, comments

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,11 @@ import 'package:betterschool/routing/routes.dart';
 import 'package:betterschool/ui/home_navbar/bloc/home_bloc.dart';
 import 'package:betterschool/ui/login/bloc/login_bloc.dart';
 import 'package:betterschool/ui/login/pages/login_page.dart';
-import 'package:betterschool/utils/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:remote_caching/remote_caching.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future<void> initializeRemoteCaching() async {
   final directory = await getApplicationSupportDirectory();
@@ -26,17 +24,8 @@ Future<void> initializeRemoteCaching() async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // maybe add some error stuff
   await initializeRemoteCaching();
-  // try {
-  //   await RemoteCaching.instance.init(
-  //     // defaultCacheDuration: Duration(days: 1),
-  //     verboseMode: true,
-  //     databasePath: join(await getDatabasesPath(), 'cache.db'),
-  //   );
-  // } catch (e) {
-  //   // imagine you got error handling
-  //   logger.e(e.toString());
-  // }
 
   initDependencies();
 
