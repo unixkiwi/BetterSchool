404f02771e5ca7f699fb33a945efaad932907add
unixkiwi <unixkiwi77@outlook.com>
2025-11-14 16:52:21 +0100

    feat: added more refreshing

--- lib/ui/timetable/pages/timetable_error_page.dart
+++ lib/ui/timetable/pages/timetable_error_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:betterschool/config/constants.dart';
 import 'package:betterschool/ui/timetable/bloc/timetable_bloc.dart';
+import 'package:betterschool/utils/time_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class TimetableErrorPage extends StatelessWidget {
@@ -18,40 +22,60 @@ class TimetableErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: SvgPicture.asset(
-                _getSvgPath(errorType),
-                width: MediaQuery.of(context).size.width * 3 / 5,
-                fit: BoxFit.contain,
-              ),
+      child: RefreshIndicator(
+        onRefresh: () async {
+          final completer = Completer<void>();
+
+          context.read<TimetableBloc>().add(
+            TimetableRefreshEvent(
+              weekString: WeekString.fromDate(DateTime.now()),
+              completer: completer,
             ),
-            Theme(
-              data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-              child: ExpansionTile(
-                title: Text(
-                  title,
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    color: Theme.of(context).colorScheme.error,
-                  ),
+          );
+
+          try {
+            await completer.future.timeout(const Duration(seconds: 30));
+          } catch (_) {}
+        },
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          // allow pull-to-refresh even when content is short
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: SvgPicture.asset(
+                  _getSvgPath(errorType),
+                  width: MediaQuery.of(context).size.width * 3 / 5,
+                  fit: BoxFit.contain,
                 ),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      description,
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Theme.of(context).colorScheme.error,
-                      ),
+              ),
+              Theme(
+                data: Theme.of(
+                  context,
+                ).copyWith(dividerColor: Colors.transparent),
+                child: ExpansionTile(
+                  title: Text(
+                    title,
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Theme.of(context).colorScheme.error,
                     ),
                   ),
-                ],
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        description,
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.error,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
