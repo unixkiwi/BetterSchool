2283eeed6154a44812aa4cd89ba53f89c1d39de2
unixkiwi <unixkiwi77@outlook.com>
2025-11-25 17:17:49 +0100

    feat: added colors for grades page

---
+++ lib/ui/core/themes/grades_page_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor invalidGradeBadgeColor = Colors.grey;
+const MaterialColor badGradeBadgeColor = Colors.red;
+const MaterialColor mediumGradeBadgeColor = Colors.orange;
+const MaterialColor decentGradeBadgeColor = Colors.yellow;
+const MaterialColor goodGradeBadgeColor = Colors.green;
