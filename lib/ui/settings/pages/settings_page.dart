0870567224997c7d7c98b34b8c244a4b5405a5f6
unixkiwi <unixkiwi77@outlook.com>
2025-12-02 18:28:37 +0100

    feat: updated settings page

--- lib/ui/settings/pages/settings_page.dart
+++ lib/ui/settings/pages/settings_page.dart
@@ -1,10 +1,90 @@
+import 'package:betterschool/ui/settings/bloc/settings_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:settings_tiles/settings_tiles.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  final Map<ThemeMode, String> _themeNameMap = {
+    ThemeMode.system: 'System Default',
+    ThemeMode.light: 'Light',
+    ThemeMode.dark: 'Dark',
+  };
+
+  final Map<ThemeMode, IconData> _themeIconMap = {
+    ThemeMode.system: Icons.brightness_auto_rounded,
+    ThemeMode.light: Icons.light_mode_rounded,
+    ThemeMode.dark: Icons.dark_mode_rounded,
+  };
+
+  SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Settings Page"));
+    return BlocProvider<SettingsBloc>.value(
+      value: context.read<SettingsBloc>(),
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(title: Text("Settings")),
+            body: ListView(
+              children: [
+                SettingSection(
+                  title: SettingSectionTitle("Look & Feel"),
+                  tiles: [
+                    SettingSingleOptionTile<String>(
+                      icon: SettingTileIcon(
+                        _themeIconMap[state.brightnessMode]!,
+                      ),
+                      title: Text('Brightness Mode'),
+                      value: SettingTileValue(
+                        _themeNameMap[state.brightnessMode]!,
+                      ),
+                      description: Text('Select the app\'s brightness mode'),
+                      dialogTitle: 'Brightness Mode',
+                      options: [
+                        _themeNameMap[ThemeMode.system]!,
+                        _themeNameMap[ThemeMode.light]!,
+                        _themeNameMap[ThemeMode.dark]!,
+                      ],
+                      initialOption: _themeNameMap[state.brightnessMode],
+                      onSubmitted: (value) {
+                        context.read<SettingsBloc>().add(
+                          ChangeThemeModeEvent(
+                            _themeNameMap.entries
+                                .firstWhere((entry) => entry.value == value)
+                                .key,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                SettingSection(
+                  title: SettingSectionTitle("Grades"),
+                  tiles: [
+                    SettingSwitchTile(
+                      icon: SettingTileIcon(Icons.calculate_rounded),
+                      title: Text('Use Grade Modifiers'),
+                      description: Text(
+                        'Include + and - modifiers in grade calculations',
+                      ),
+                      value: SettingTileValue(
+                        state.useModifiers ? 'On' : 'Off',
+                      ),
+                      onChanged: (value) {
+                        context.read<SettingsBloc>().add(
+                          ChangeUseModifiersEvent(value),
+                        );
+                      },
+                      toggled: state.useModifiers,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
