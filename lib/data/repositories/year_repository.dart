6186001b4be0271f80fc8688c2de3bb4fd5ba8fc
unixkiwi <unixkiwi77@outlook.com>
2026-01-26 16:02:27 +0100

    feat(year): implement caching for school years retrieval from API

--- lib/data/repositories/year_repository.dart
+++ lib/data/repositories/year_repository.dart
@@ -4,18 +4,37 @@ import 'package:betterschool/domain/models/year.dart';
 import 'package:betterschool/utils/logger.dart';
 import 'package:betterschool/utils/result.dart';
 import 'package:dio/dio.dart';
+import 'package:remote_caching/remote_caching.dart';
 
 class YearRepository {
   final BesteSchuleApiClientImpl _apiClient;
+  final _cacheKey = "school_years_cache";
 
   YearRepository(this._apiClient);
 
   Future<Result<List<SchoolYear>>> getYears() async {
+    logger.i("Fetching school years from the API...");
+
     try {
-      BesteSchuleApiResponse<List<YearModel>> response = await _apiClient
-          .getYears();
+      BesteSchuleApiResponse<List<YearModel>> response = await RemoteCaching
+          .instance
+          .call(
+            _cacheKey,
+            remote: _apiClient.getYears,
+            cacheDuration: const Duration(hours: 12),
+            fromJson: (json) =>
+                BesteSchuleApiResponse<List<YearModel>>.fromJson(
+                  json as Map<String, dynamic>,
+                  (data) =>
+                      (data as List).map((e) => YearModel.fromJson(e)).toList(),
+                ),
+          );
 
       if (response.data != null) {
+        logger.i(
+          "Received ${response.data!.length} school years from the API.",
+        );
+
         List<YearModel> data = response.data!;
         List<SchoolYear> result = data
             .map(
@@ -30,6 +49,10 @@ class YearRepository {
 
         return Result.success(result);
       } else {
+        logger.e(
+          "Received data from the API is null!\nresponse.data = ${(response.data == null).toString()}",
+        );
+
         return Result.error(
           Exception(
             "Received data from the API is null!\nresponse.data = ${(response.data == null).toString()}",
